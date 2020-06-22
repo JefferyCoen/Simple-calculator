8fe4d9cc27a483d79cbeb914f5c879ee7752c5ba
Jeffery_Coen <coen.jeffery@outlook.com>
2020-06-22 00:23:41 -0700

    updated layout with del feature

--- main.js
+++ main.js
@@ -2,15 +2,21 @@ function insert(num) {
     document.resultWindow.resultText.value = document.resultWindow.resultText.value+num
 }
 
+
+function clr() {
+    document.resultWindow.resultText.value = "";
+}
+
+function del() {
+    var exp = document.resultWindow.resultText.value;
+    document.resultWindow.resultText.value = exp.substring(0, exp.length - 1)
+}
+
 function equals() {
     var exp = document.resultWindow.resultText.value;
     if(exp){
         document.resultWindow.resultText.value = eval(exp)
     }
-}
-
-function clr() {
-    document.resultWindow.resultText.value="";
 }
 
 function sqrRoot() {
