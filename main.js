438a1be0bc7da97a3a6dc63cc6c4312dc5549261
Jeffery_Coen <coen.jeffery@outlook.com>
2020-06-22 14:38:04 -0700

    started BMI Calculator

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//For Basic Calculator
 function insert(num) {
     document.resultWindow.resultText.value = document.resultWindow.resultText.value+num
 }
@@ -22,4 +23,15 @@ function equals() {
 function sqrRoot() {
     var squareRoot = Math.sqrt(document.resultWindow.resultText.value)
     document.resultWindow.resultText.value = squareRoot
+}
+
+//For BMI Calculator
+function hideMetric() {
+    document.getElementById("metricForm").style.visibility="hidden";
+    document.getElementById("USForm").style.visibility="visible";
+}
+
+function hideUS() {
+    document.getElementById("metricForm").style.visibility="visible";
+    document.getElementById("USForm").style.visibility="hidden";
 }
