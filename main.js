a5e818ea72c05035c68dc07ae904241ba0431e56
Jeffery_Coen <coen.jeffery@outlook.com>
2020-06-25 09:55:46 -0700

    added metric calcualation functions

--- main.js
+++ main.js
@@ -35,3 +35,31 @@ function hideUS() {
     document.getElementById("metricForm").style.visibility="visible";
     document.getElementById("USForm").style.visibility="hidden";
 }
+
+function calMetric() {
+    var mValue1 = document.getElementById('kg').value;
+    var mValue2 = document.getElementById('cm').value;
+    // calculate BMI 
+    var bmiMetric = mValue1/mValue2/mValue2*10000;
+    //round number to 1 decimal place
+    var bmiRounded =bmiMetric.toFixed(1);
+
+    document.getElementById('yourBMI').innerHTML = "Your BMI Is: " + bmiRounded; 
+
+    //For converting to U.S. Measurements
+    //weight
+    var con1 = (mValue1 * 2.2).toFixed(3)
+    //round number to 1 decimal
+    var conRounded = (mValue1 * 2.2).toFixed(1)
+    //height
+    var con2 = (mValue2 / 2.54).toFixed(0)
+
+    //additional information
+    console.log('Your Actual BMI: ' + bmiMetric)
+    console.log('Your BMI Rounded to Nearest First Decimal: ' + bmiRounded)
+    console.log('Your Converted Weight in lbs: ' + con1)
+    console.log('Your Converted Hieght in Inches: ' + con2)
+
+    document.getElementById('conversions').innerHTML ='Your Converted Weight in U.S. Standard is: ' + conRounded;
+
+}
